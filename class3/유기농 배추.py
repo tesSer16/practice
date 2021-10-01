33fd5704d2aa56739e8c0eb09de794abc76a9985
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-01 16:18:47 +0900

    흠... 이해는 됨

--- class3/유기농 배추.py
+++ class3/유기농 배추.py
@@ -1,6 +1,21 @@
 import sys
 input = sys.stdin.readline
 
+
+def chain(a, b):
+    if a < 0 or a >= N or b < 0 or b >= M:
+        return
+    if not ground[a][b]:
+        return
+
+    ground[a][b] = 0
+
+    chain(a - 1, b)
+    chain(a + 1, b)
+    chain(a, b + 1)
+    chain(a, b - 1)
+
+
 T = int(input())
 for _ in range(T):
     M, N, K = map(int, input().split())
@@ -9,8 +24,11 @@ for _ in range(T):
         x, y = map(int, input().split())
         ground[y][x] = 1
 
+    answer = 0
+    for i in range(N):
+        for j in range(M):
+            if ground[i][j]:
+                chain(i, j)
+                answer += 1
 
-for i in range(N):
-    for j in range(M):
-        print(ground[i][j], end=" ")
-    print()
+    print(answer)
