88b1df03af63de0ef79bb52403519d13848ae571
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-04 05:33:05 +0900

    클래스로 한번 해봄

--- class3/four squares.py
+++ class3/four squares.py
@@ -1,13 +1,16 @@
-def solve(num, cnt):
-    global result
-    for a in range(int(num**0.5), int((num//(4 - cnt + 1))**0.5), -1):
-        if num == a*a:
-            result = min(cnt, result)
-        else:
-            solve(num - a*a, cnt + 1)
+class Solve:
+    def __init__(self):
+        self.result = 4
+        n = int(input())
+        self.solve(n, 1)
+
+    def solve(self, num, cnt):
+        for a in range(int(num**0.5), int((num//(4 - cnt + 1))**0.5), -1):
+            if num == a*a:
+                self.result = min(cnt, self.result)
+            else:
+                self.solve(num - a*a, cnt + 1)
 
 
-n = int(input())
-result = 4
-solve(n, 1)
-print(result)
+answer = Solve()
+print(answer.result)
