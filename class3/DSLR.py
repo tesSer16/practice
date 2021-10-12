8b7874ae35334ff17e645cd5c450bf695a8d4665
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-12 21:17:17 +0900

    조금 수정해봄

--- class3/DSLR.py
+++ class3/DSLR.py
@@ -2,39 +2,34 @@ from collections import deque
 import sys
 
 
-def D(n):
-    return (2 * n) % 10000
-
-
-def S(n):
-    return (n - 1) % 10000
-
-
-def L(n):
-    return (n % 1000) * 10 + n // 1000
-
-
-def R(n):
-    return (n % 10) * 1000 + n // 10
-
-
 def bfs(a, b):
     q = deque([a])
     while q:
         n = q.popleft()
         if n == b:
-            return visited[n]
+            return
 
         for k, v in commands.items():
             value = v(n)
-            if visited[value] == "1":
+            if visited[value] == 0:
                 q.append(value)
-                visited[value] = visited[n] + k
+                visited[value] = (n, k)
 
 
-commands = {'D': D, 'S': S, 'L': L, 'R': R}
+commands = {'D': lambda x: (2 * x) % 10000,
+            'S': lambda x: (x - 1) % 10000,
+            'L': lambda x: (x % 1000) * 10 + x // 1000,
+            'R': lambda x: (x % 10) * 1000 + x // 10}
 for _ in range(int(input())):
-    visited = ["1"] * 100001
+    visited = [0] * 10001
     A, B = map(int, sys.stdin.readline().split())
-    visited[A] = ""
-    print(bfs(A, B))
+    visited[A] = (0, '')
+    bfs(A, B)
+    answer = []
+    while B != A:
+        answer.append(visited[B][1])
+        B = visited[B][0]
+
+    for _ in range(len(answer)):
+        print(answer.pop(), end="")
+    print()
