336809f7df57c129365f0189320db73347647ce0
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-13 23:39:52 +0900

    다익스트라 인접행렬로도 해봄

--- class3/뱀과 사다리 게임.py
+++ class3/뱀과 사다리 게임.py
@@ -9,21 +9,21 @@ def dijkstra(start):
         distance, current = heapq.heappop(heap)
         if d[current] < distance:
             continue
-        for data in nodes[current]:
-            next_distance = distance + data[1]
-            if next_distance < d[data[0]]:
-                d[data[0]] = next_distance
-                heapq.heappush(heap, (next_distance, data[0]))
+        for idx in range(100):
+            next_distance = distance + graph[current][idx]
+            if next_distance < d[idx]:
+                d[idx] = next_distance
+                heapq.heappush(heap, (next_distance, idx))
 
     return d
 
 
 N, M = map(int, input().split())
-nodes = [[] for _ in range(100)]
+graph = [[0 if i == j else 100 for j in range(100)] for i in range(100)]
 ladder_snake = []
 for _ in range(N + M):
     x, y = map(int, input().split())
-    nodes[x - 1].append([y - 1, 0])
+    graph[x - 1][y - 1] = 0
     ladder_snake.append(x - 1)
 
 for i in range(100):
@@ -32,6 +32,6 @@ for i in range(100):
     for j in range(1, 7):
         if i + j > 99:
             continue
-        nodes[i].append([i + j, 1])
+        graph[i][i + j] = 1
 
 print(dijkstra(0)[99])
