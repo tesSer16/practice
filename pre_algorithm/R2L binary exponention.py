48b903ecb78c965b0c85e78262607af8dbc95ebb
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-17 02:59:53 +0900

    1629 날먹

--- pre_algorithm/R2L binary exponention.py
+++ pre_algorithm/R2L binary exponention.py
@@ -4,17 +4,13 @@ def R2L(g, k, N):
 
     A, B = 1, g
     k = format(k, 'b')
-    t = len(k) - 1
 
-    for i in range(t + 1):
-        if k[i] == '1':
+    for ki in k[::-1]:
+        if ki == '1':
             A = (A * B) % N
         B = (B * B) % N
 
     return A
 
 
-g = int(input())
-k = int(input())
-N = int(input())
-print(R2L(g, k, N))
+print(R2L(*map(int, input().split())))
