f98b73b6f5469b2331343fc7f6e369fade3ddf9f
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-18 00:38:14 +0900

    bfs는 간단하긴 한데 조금 느리네 그래도 했다

--- class4/치즈.py
+++ class4/치즈.py
@@ -1,40 +1,84 @@
 import copy
+import sys
+sys.setrecursionlimit(10**5)
 
 
-def check(x, y):
+def check(r, c):
     contacted = 0
-    for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N and not board[nx][ny]:
+    for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        nr, nc = r + dr, c + dc
+        if not board[nr][nc] and visited[nr][nc] == -1:
             contacted += 1
 
     return contacted
+
+
+def void(r, c, value):
+    visited[r][c] = value
+    void_temp.append((r, c))
+    for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < N and 0 <= nc < M and not board[nr][nc] and not visited[nr][nc]:
+            void(nr, nc, value)
+
+
+def delete_void(value):
+    for v in void_list[-value - 1]:
+        r, c = v
+        for dr, dc in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            nr, nc = r + dr, c + dc
+            if board[nr][nc]:
+                cheese[nr][nc] += 1
+                if cheese[nr][nc] >= 2 and (nr, nc) not in q:
+                    temp.add((nr, nc))
+
+        visited[r][c] = -1
 
 
 N, M = map(int, input().split())
 
 board = [list(map(int, input().split())) for _ in range(N)]
 cheese = copy.deepcopy(board)
-q = []
+visited = [[0] * M for _ in range(N)]
+val = -1
+void_list = []
+for i in range(N):
+    for j in range(M):
+        if not board[i][j] and not visited[i][j]:
+            void_temp = []
+            void(i, j, val)
+            void_list.append(void_temp)
+            val -= 1
+
+
+q = set()
 for i in range(N):
     for j in range(M):
         if board[i][j]:
             temp = check(i, j)
             cheese[i][j] = temp
             if temp >= 2:
-                q.append((i, j))
+                q.add((i, j))
 
-print(*cheese, sep='\n')
 hour = 0
-temp = []
 while q:
-    x, y = q.pop()
-    board[x][y] = cheese[x][y] = 0
-    for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-        nx, ny = x + dx, y + dy
-        if 0 <= nx < N and 0 <= ny < N and board[nx][ny]:
-            cheese[nx][ny] += 1
-            if cheese[nx][ny] >= 2:
-                temp.append((nx, ny))
+    # print(*cheese, sep='\n')
+    # print(q)
+    # print()
+    temp = set()
+    for x, y in q:
+        for dx, dy in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
+            nx, ny = x + dx, y + dy
+            if board[nx][ny] and (nx, ny) not in q:
+                cheese[nx][ny] += 1
+                if cheese[nx][ny] >= 2:
+                    temp.add((nx, ny))
+            elif visited[nx][ny] < -1:
+                delete_void(visited[nx][ny])
+
+        board[x][y] = cheese[x][y] = 0
+
+    q = temp
+    hour += 1
 
 print(hour)
