d5f3283159fea013b303e66edf48ef3fb793a1bb
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-20 17:16:59 +0900

    처음부터 맞았던 건데...

--- 선린 가을맞이/F.py
+++ 선린 가을맞이/F.py
@@ -1,12 +1,10 @@
-import sys
+N = 1000000
+dp = [[0, 0] for _ in range(N)]
+dp[0][0], dp[0][1] = 3, 4
+for i in range(1, N):
+    dp[i][0] = (3 * dp[i - 1][0] + dp[i - 1][1]) % 1000000007
+    dp[i][1] = (4 * dp[i - 1][0] + 2 * dp[i - 1][1]) % 1000000007
 
 for _ in range(int(input())):
-    N = int(sys.stdin.readline())
-    dp = [[0 for _ in range(N)], [0 for _ in range(N)]]
-    dp[0][0], dp[1][0] = 3, 4
-    for i in range(1, N):
-        temp = ((dp[0][i - 1] << 2) + (dp[1][i - 1] << 1)) % 1000000007
-        dp[1][i] += temp
-        dp[0][i] += (dp[0][i - 1] + (temp >> 1)) % 1000000007
-
-    print((dp[0][-1] + dp[1][-1]) % 1000000007)
+    idx = int(input()) - 1
+    print(sum(dp[idx]) % 1000000007)
