9f74faabed9d7566cbef31f22730a4f222e8df51
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-22 02:42:18 +0900

    행렬제곱을 대각화로 풀어보겠읍니다..

---
+++ class4/행렬 제곱.py
@@ -0,0 +1,13 @@
+def R2L(g, k, N):
+    if g == 0:
+        return 0
+
+    A, B = 1, g
+    k = format(k, 'b')
+
+    for ki in k[::-1]:
+        if ki == '1':
+            A = (A * B) % N
+        B = (B * B) % N
+
+    return A
