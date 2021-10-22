080e16771e7075f1dacb9c12d9d9bef3de59e94f
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-22 22:26:36 +0900

    1트

--- class4/행렬 제곱.py
+++ class4/행렬 제곱.py
@@ -1,13 +1,32 @@
-def R2L(g, k, N):
+def R2L(g, k):
     if g == 0:
         return 0
 
-    A, B = 1, g
+    A, B = [[1 if i == j else 0 for j in range(n)] for i in range(n)], g
     k = format(k, 'b')
 
     for ki in k[::-1]:
         if ki == '1':
-            A = (A * B) % N
-        B = (B * B) % N
+            A = matrix_product(A, B)
+        B = matrix_product(B, B)
 
     return A
+
+
+def matrix_product(A, B):
+    result = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                result[i][j] += (A[i][k] * B[k][j]) % 1000
+
+    return result
+
+
+n, b = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+answer = R2L(matrix, b)
+for ans in answer:
+    for a in ans:
+        print(a % 1000, end=" ")
+    print()
