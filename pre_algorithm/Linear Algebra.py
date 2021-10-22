cea948e9cd19b318494251070e1663446fba413c
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-22 18:20:03 +0900

    역행렬 구현했읍니다

--- pre_algorithm/Linear Algebra.py
+++ pre_algorithm/Linear Algebra.py
@@ -27,9 +27,46 @@ class Matrix:
 
         return Matrix(result)
 
+    def change_row(self, r1, r2):
+        self.matrix[r1], self.matrix[r2] = self.matrix[r2], self.matrix[r1]
+
+    def scalar(self, k):
+        for i in range(self.rows):
+            for j in range(self.columns):
+                self.matrix[i][j] *= k
+
+    def scalar_row(self, row, k):
+        for j in range(self.columns):
+            self.matrix[row][j] *= k
+
     def inverse(self):
-        if self.det() == 0:
-            return False
+        origin = Matrix(self.matrix)
+        inv = Matrix.eye(self.rows)
+        for j in range(self.columns):
+            for i in range(j, self.columns):
+                if origin.matrix[i][j]:
+                    break
+            else:
+                return False
+
+            origin.change_row(j, i)
+            inv.change_row(j, i)
+
+            k = origin.matrix[j][j]
+            origin.scalar_row(j, 1 / k)
+            inv.scalar_row(j, 1 / k)
+
+            target_row1 = inv.matrix[j]
+            target_row2 = origin.matrix[j]
+            for r in range(self.rows):
+                if r == j:
+                    continue
+                k = origin.matrix[r][j]
+                for c in range(self.columns):
+                    inv.matrix[r][c] -= k * target_row1[c]
+                    origin.matrix[r][c] -= k * target_row2[c]
+
+        return inv
 
     def __add__(self, other):
         # return Matrix([[self.matrix[i][j] + other.matrix[i][j] for j in range(self.columns)] for i in range(self.rows)])
@@ -51,7 +88,8 @@ class Matrix:
 
 
 if __name__ == "__main__":
-    A = Matrix([[1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9]])
-    print(A.det())
+    A = Matrix([[5, 8, -4],
+                [6, 9, -5],
+                [4, 7, -2]])
+    inv_A = A.inverse()
+    print(*inv_A.matrix, sep='\n')
