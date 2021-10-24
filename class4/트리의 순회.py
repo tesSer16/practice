7923c496b4b08d9917c7e84c123e55c570e59c6a
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-24 13:06:42 +0900

    Og: 순회를 최소화 하자!

--- class4/트리의 순회.py
+++ class4/트리의 순회.py
@@ -1,25 +1,23 @@
-def divide(_in, _post):
-    if not _post:
+import sys
+sys.setrecursionlimit(10**6)
+
+
+def divide(in_i, in_f, post_i, post_f):
+    if post_i > post_f or in_i > in_f:
         return
-    root = _post[-1]
-    print(root, end=" ")
-    if _post[0] == root:
-        return
+    root = postorder[post_f]
+    print(root, end=' ')
 
-    root_index = _in.index(root)
-    left_in = _in[:root_index]
-    right_in = _in[root_index + 1:]
-
-    boundary = -len(right_in) - 1
-    left_post = _post[:boundary]
-    right_post = _post[boundary:-1]
-
-    divide(left_in, left_post)
-    divide(right_in, right_post)
+    len_right = in_f - pos[root]
+    divide(in_i, pos[root] - 1, post_i, post_f - len_right - 1)
+    divide(pos[root] + 1, in_f, post_f - len_right, post_f - 1)
 
 
 n = int(input())
 inorder = list(map(int, input().split()))
 postorder = list(map(int, input().split()))
+pos = [0] * (n + 1)
+for i in range(n):
+    pos[inorder[i]] = i
 
-divide(inorder, postorder)
+divide(0, n - 1, 0, n - 1)
