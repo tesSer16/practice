64fb6b3336590b946e48a938764ca4435842afa4
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-10-29 22:48:36 +0900

    음 배우긴 했다.

---
+++ class5/LCS 2.py
@@ -0,0 +1,38 @@
+def LCS_length():
+    result = 0
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            if string1[i] == string2[j]:
+                LCS[i][j] = LCS[i - 1][j - 1] + 1
+                result = max(result, LCS[i][j])
+            else:
+                LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
+
+    return result
+
+
+def LCS_track():
+    result = []
+    i, j = N, M
+    value = LCS[i][j]
+    while LCS[i][j]:
+        if value == LCS[i - 1][j]:
+            i -= 1
+        elif value == LCS[i][j - 1]:
+            j -= 1
+        else:
+            result.append(string1[i])
+            i, j = i - 1, j - 1
+            value = LCS[i][j]
+
+    return result[::-1]
+
+
+if __name__ == "__main__":
+    string1 = [''] + list(input())
+    string2 = [''] + list(input())
+    N, M = len(string1) - 1, len(string2) - 1
+    LCS = [[0 for _ in range(M + 1)] for _ in range(N + 1)]
+
+    print(LCS_length())
+    print(''.join(LCS_track()))
