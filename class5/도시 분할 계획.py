422299a56fc271c19d491478a72d965cdd0b4175
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-11-03 20:13:34 +0900

    kruskal: E*log(E) prim: E * log(V) kruskal faster than prim?!

--- class5/도시 분할 계획.py
+++ class5/도시 분할 계획.py
@@ -2,9 +2,11 @@ import sys
 
 
 def find(x):
-    if parent[x] != x:
+    if parent[x] == x:
+        return x
+    else:
         parent[x] = find(parent[x])
-    return parent[x]
+        return parent[x]
 
 
 def union(x, y):
@@ -27,6 +29,7 @@ parent = [i for i in range(V + 1)]
 rank = [0] * (V + 1)
 data = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(E)], key=lambda x: -x[2])
 total_w = edges = 0
+last_C = data[0][2]
 while edges < V - 1:
     A, B, C = data.pop()
     if find(A) == find(B):
@@ -35,5 +38,6 @@ while edges < V - 1:
     union(A, B)
     total_w += C
     edges += 1
+    last_C = C
 
-print(total_w)
+print(total_w - last_C)
