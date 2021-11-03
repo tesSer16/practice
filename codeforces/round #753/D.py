0a35ecce320510e3002f7d9bab06a36ddda12959
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-11-03 18:53:19 +0900

    포기요

--- codeforces/round #753/D.py
+++ codeforces/round #753/D.py
@@ -1,16 +1,14 @@
 import sys
 read = sys.stdin.readline
 
-for _ in range(int(input())):
-    n = int(read())
-    nums = list(map(int, read().split()))
-    commands = read().strip()
-    sorted_data = sorted([(k, v) for k, v in zip(nums, commands)], key=lambda x: (x[1], x[0]))
 
+def solve():
     check = [0] * n
     i = n - 1
     while sorted_data[i][1] != 'B':
         num = sorted_data[i][0]
+        if num > n:
+            return False
         bound = num if num > 0 else 1
         for nu in range(n, bound - 1, -1):
             if not check[nu - 1]:
@@ -23,13 +21,24 @@ for _ in range(int(input())):
 
     for j in range(i + 1):
         num = sorted_data[j][0]
+        if num < 1:
+            return False
         bound = num + 1 if num < n else n + 1
         for nu in range(1, bound):
             if not check[nu - 1]:
                 check[nu - 1] = 1
                 break
 
-    if all(check):
+    return all(check)
+
+
+for _ in range(int(input())):
+    n = int(read())
+    nums = list(map(int, read().split()))
+    commands = read().strip()
+    sorted_data = sorted([(k, v) for k, v in zip(nums, commands)], key=lambda x: (x[1], x[0]))
+
+    if solve():
         print("YES")
     else:
         print("NO")
