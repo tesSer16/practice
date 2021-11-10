47a2d5886d8c134be9555599ba1f8b28807a08dd
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-11-10 09:40:15 +0900

    즐겁다!

--- class5/낚시왕.py
+++ class5/낚시왕.py
@@ -1,55 +1,81 @@
+from copy import deepcopy
+
+
 class Shark:
-    def __init__(self, row, col, spd, dir, size):
+    def __init__(self, row, col, spd, dir):
         self.row = row
         self.col = col
-        self.spd = spd % (2 * (row - 1, col - 1)[(dir - 1) // 2])
+        self.spd = spd % (2 * (R - 1, C - 1)[(dir - 1) // 2])
         self.dir = dir
-        self.size = size
 
     def move(self, v):
         if self.dir == 1:
             if v < self.row:
                 self.row -= v
             else:
-                self.row = 0
+                temp, self.row = self.row, 0
                 self.dir = 2
-                self.move(v - self.row)
+                self.move(v - temp)
 
-        if self.dir == 2:
+        elif self.dir == 2:
             if v < (R - 1 - self.row):
                 self.row += v
             else:
-                self.row = R - 1
+                temp, self.row = self.row, R - 1
                 self.dir = 1
-                self.move(v - (R - 1 - self.row))
+                self.move(v - (R - 1 - temp))
 
-        if self.dir == 3:
+        elif self.dir == 4:
             if v < self.col:
                 self.col -= v
             else:
-                self.col = 0
-                self.dir = 4
-                self.move(v - self.col)
+                temp, self.col = self.col, 0
+                self.dir = 3
+                self.move(v - temp)
 
-        if self.dir == 4:
+        elif self.dir == 3:
             if v < (C - 1 - self.col):
                 self.col += v
             else:
-                self.col = C - 1
-                self.dir = 3
-                self.move(v - (C - 1 - self.col))
+                temp, self.col = self.col, C - 1
+                self.dir = 4
+                self.move(v - (C - 1 - temp))
 
 
 R, C, M = map(int, input().split())
-sharks = []
-for _ in range(M):
-    sharks.append(Shark(*map(int, input().split())))
+sharks = {}
+board = [[0] * C for _ in range(R)]
 
+for _ in range(M):
+    r, c, s, d, z = map(int, input().split())
+    sharks[z] = Shark(r - 1, c - 1, s, d)
+    board[r - 1][c - 1] = z
+
+total_size = 0
 for j in range(C):
     # catch sharks
+    for i in range(R):
+        if board[i][j]:
+            total_size += board[i][j]
+            del sharks[board[i][j]]
+            board[i][j] = 0
+            break
 
     # move each sharks
-    for shark in sharks:
+    new_board = [[0] * C for _ in range(R)]
+    terminate = []
+    for size, shark in sharks.items():
         shark.move(shark.spd)
-    # remove intersect
-    pass
+        if new_board[shark.row][shark.col]:
+            if new_board[shark.row][shark.col] > size:
+                terminate.append(size)
+            else:
+                terminate.append(new_board[shark.row][shark.col])
+                new_board[shark.row][shark.col] = size
+        else:
+            new_board[shark.row][shark.col] = size
+
+    for t in terminate:
+        del sharks[t]
+    board = deepcopy(new_board)
+print(total_size)
