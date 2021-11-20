6bd0d5b98842725b95ebf96e388bcd7ecf654cbe
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-11-20 13:52:57 +0900

    처음으로 재채점돼서 틀림 ㅋㅋㅋ

--- temp/선분 교차 3.py
+++ temp/선분 교차 3.py
@@ -21,7 +21,7 @@ def solution(x1, y1, x2, y2, x3, y3, x4, y4):
                 return
 
         elif a * (x3 - x1) == - b * (y3 - y1):
-            if x1 == x3:
+            if x1 == x2:
                 s1, s2, s3, s4 = y1, y2, y3, y4
             else:
                 s1, s2, s3, s4 = x1, x2, x3, x4
