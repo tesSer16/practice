603a76e04e2c30afc953dd4c7c0da6896b2df87c
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-11-25 15:59:54 +0900

    이 1등을 C++ 코더님께 바칩니다 ㅠㅠ

--- class5/펠린드롬 분할 - N^2+.py
+++ class5/펠린드롬 분할 - N^2+.py
@@ -1,14 +1,10 @@
-s = ' ' + input()
-n = len(s) - 1
-d, p = [0] * (n + 1), [0] * (n + 1)
-for i in range(1, n + 1):
-    d[i] = i
-    for j in range(1, i + 1):
-        if s[j] == s[i] and (i - j <= 1 or p[j + 1]):
-            p[j] = 1
-            d[i] = min(d[i], d[j - 1] + 1)
-        else:
-            p[j] = 0
-    print(p[1:i + 1])
-
-print(d[n])
+s=' '+input()
+n=len(s)
+d=[0]*n
+p=[0]*n
+for i in range(n):
+    d[i]=i
+    for j in range(i+1):
+        if s[j]==s[i]and(i-j<=1or p[j+1]):p[j]=1;d[i]=min(d[i],d[j-1]+1)
+        else:p[j]=0
+print(d[i])
