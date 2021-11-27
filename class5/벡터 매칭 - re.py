55224c1ba0bc2e980cb5a1aae057fead7bf695c8
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2021-11-27 22:25:43 +0900

    조금 바꿈

--- class5/벡터 매칭 - re.py
+++ class5/벡터 매칭 - re.py
@@ -2,14 +2,14 @@ from itertools import combinations
 import sys
 read = sys.stdin.readline
 
+
+def cal(x, y):
+    return ((2 * sum(x) - sum_x)**2 + (2 * sum(y) - sum_y)**2)**0.5
+
+
 for _ in range(int(read())):
     N = int(read())
     data = [list(map(int, read().split())) for _ in range(N)]
     sum_x, sum_y = (lambda x: (sum(x[0]), sum(x[1])))(list(zip(*data)))
     answer = float('inf')
-
-    for case in combinations(data, N // 2):
-        x1, y1 = (lambda x: (sum(x[0]), sum(x[1])))(list(zip(*case)))
-        answer = min(answer, ((2 * x1 - sum_x)**2 + (2 * y1 - sum_y)**2)**0.5)
-
-    print(answer)
+    print(min([cal(*zip(*case)) for case in combinations(data, N // 2)]))
