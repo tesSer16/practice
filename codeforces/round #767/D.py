0cd501dce7e0bdfe7a17c6d48f00b1aa88c64ac5
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2022-01-23 14:04:03 +0900

    뭐냐고~~ 할만 했네

--- codeforces/round #767/D.py
+++ codeforces/round #767/D.py
@@ -4,19 +4,19 @@ read = sys.stdin.readline
 
 def check(arr):
     check2 = [[0] * 26 for _ in range(26)]
+    check23 = [[0] * 26 for _ in range(26)]
     check3 = [[[0] * 26 for _ in range(26)] for _ in range(26)]
     for i in range(n):
-        if arr[i] == arr[i][::-1]:
+        if len(arr[i]) == 1:
             return True
         if len(arr[i]) == 2:
             a, b = arr[i]
             check2[ord(a) - 97][ord(b) - 97] = 1
-            if check2[ord(b) - 97][ord(a) - 97]:
+            if check2[ord(b) - 97][ord(a) - 97] or check23[ord(b) - 97][ord(a) - 97]:
                 return True
         else:
             a, b, c = arr[i]
-            check2[ord(a) - 97][ord(b) - 97] = 1
-            check2[ord(b) - 97][ord(c) - 97] = 1
+            check23[ord(a) - 97][ord(b) - 97] = 1
             check3[ord(a) - 97][ord(b) - 97][ord(c) - 97] = 1
             if check2[ord(c) - 97][ord(b) - 97] or check3[ord(c) - 97][ord(b) - 97][ord(a) - 97]:
                 return True
