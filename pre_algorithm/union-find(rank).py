12aeca80c3d53768475bbd1bb6a8c6e9577cb6f3
tesSer16 <80532804+tesSer16@users.noreply.github.com>
2022-05-28 19:34:43 +0900

    이걸 하네 ㅋㅋ

---
+++ pre_algorithm/union-find(rank).py
@@ -0,0 +1,22 @@
+def find(x):
+    while x != root[x]:
+        x = root[x]
+    return x
+
+
+def union(x, y):
+    root_x = find(x)
+    root_y = find(y)
+    if root_x != root_y:
+        if rank[root_x] > root_y[root_y]:
+            root[root_y] = root_x
+        elif rank[root_y] > rank[root_x]:
+            root[root_x] = root_y
+        else:
+            root[root_y] = root_x
+            rank[root_x] += 1
+
+
+N = 10
+root = [i for i in range(N)]
+rank = [0 for _ in range(N)]
